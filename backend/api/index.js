2468923b8d3c612195c7106533c54ce0f3fc5d0d
Rhythm Garg <rhythmgarg05@gmail.com>
2023-08-24 02:09:43 +0530

    no idea what I did

---
+++ backend/api/index.js
@@ -0,0 +1,3 @@
+const server = require('../backend/server')
+
+export default server;
--- backend/server.js
+++ backend/server.js
@@ -169,3 +169,5 @@ app.post('/subscribe', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+export default server;
