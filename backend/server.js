c8881a6ed4a840eaf41df83ad1a8a2404ba14ecb
Rhythm Garg <rhythmgarg05@gmail.com>
2023-09-15 23:02:47 +0530

    Testing Resend implementation instead of Brevo

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,8 @@ const axios = require('axios');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const cron = require('cron');
-const nodemailer = require('nodemailer');
-const {google} = require('googleapis');
-const { type } = require('@testing-library/user-event/dist/type');
 require('dotenv').config();
-const SibApiV3Sdk = require('sib-api-v3-sdk');
+const {Resend} = require('resend');
 
 const app = express();
 const port = 5000;
@@ -59,11 +56,7 @@ const connectDatabase = async () => {
 
 connectDatabase();
 
-const defaultClient = SibApiV3Sdk.ApiClient.instance;
-const apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = process.env.BREVO_API_KEY;
-const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-let sendMail = new SibApiV3Sdk.SendSmtpEmail();
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendGFIEmail = async (repoURL) => {
   const apiUrl = `https://api.github.com/repos/${repoURL}/issues?labels=good%20first%20issue&&state=open`;
@@ -71,50 +64,43 @@ const sendGFIEmail = async (repoURL) => {
     const response = await axios.get(apiUrl);
     const issues = response.data;
 
-    const subscribed = await Subscription.find({repoURL});
+    try {
+      const subscribed = await Subscription.find({repoURL});
 
-    if(issues.length > 0 && subscribed.length > 0) {
-      let lastProcessedIssueId = subscribed[0].lastProcessedIssueId || 0;
-      const latestIssueID = issues[0].number;
+      if(issues.length > 0 && subscribed.length > 0) {
+        let lastProcessedIssueId = subscribed[0].lastProcessedIssueId || 0;
+        const latestIssueID = issues[0].number;
 
-      if(latestIssueID > lastProcessedIssueId) {
-        subscribed.forEach(async (sub) => {
-          if(sub.lastProcessedIssueId < latestIssueID) {
-            const subject = `A new GFI has been raised in ${repoURL}!`;
-            const text = `A new Good First Issue has been raised in the repository: ${repoURL}\n\nIssue Title: ${issues[0].title}\n\nIssue URL: ${(issues[0].url).replace("https://api.github.com/repos", "https://github.com/")}`;
-            const mailOptions = {
-              from: 'Rhythm from GitAlert <' + process.env.EMAIL_USER + '>',
-              to: sub.email,
-              subject: subject,
-              text: text,
-            };
+        if(latestIssueID > lastProcessedIssueId) {
+          subscribed.forEach(async (sub) => {
+            if(sub.lastProcessedIssueId < latestIssueID) {
+              const subject = `A new GFI has been raised in ${repoURL}!`;
+              const text = `A new Good First Issue has been raised in the repository: ${repoURL}\n\nIssue Title: ${issues[0].title}\n\nIssue URL: ${(issues[0].url).replace("https://api.github.com/repos", "https://github.com/")}`;
 
-            sendMail = {
-              sender: {
-                email: process.env.EMAIL_USER,
-                name: 'Rhythm Garg',
-              },
-              to: [{
-                email: sub.email
-              }],
-              subject: subject,
-              textContent : text,
-              headers: {
-                'X-Mailin-custom': 'custom_header_1:custom_value_1|custom_header_2:custom_value_2'
+              try {
+                const mail = await resend.emails.send({
+                  from: 'Rhythm from GitAlert <rhythm@gitalert.me>',
+                  to: sub.email,
+                  subject: subject,
+                  text: text,
+                });
+                console.log(mail);
+                console.log(`GFI Email sent to ${sub.email} for ${repoURL}.`);
+              } catch (error) {
+                console.log(`Couldn't send email to ${sub.email}: `, error);
               }
-            };
-            
-            await apiInstance.sendTransacEmail(sendMail).then(function(data) {
-              console.log('API called successfully. Returned data: ' + data);
-              }, function(error) {
-                  console.error(error);
-            });
 
-            sub.lastProcessedIssueId = latestIssueID;
-            await sub.save();
-          }
-        });
+              sub.lastProcessedIssueId = latestIssueID;
+              await sub.save();
+            }
+          });
+        }
       }
+    } catch (error) {
+      res.status(500).json({
+        error: `No subscribed repos found.`
+      })
+      console.log("Couldn't find subscribed repo: ", error);
     }
   } catch(error) {
     console.error('Error fetching GFIs:', error);
@@ -125,13 +111,15 @@ const repoToCheck = [];
 const addRepoToCheck = (repoURL) => {
   if(!repoToCheck.includes(repoURL)) {
     repoToCheck.push(repoURL);
+    console.log(`Added ${repoURL} to repoToCheck.`);
   }
 };
 
-const cronJob = new cron.CronJob('* * * * *', () => {
+const cronJob = new cron.CronJob('*/10 * * * * *', () => {
   console.log('Checking for new GFIs...');
   repoToCheck.forEach((repoURL) => {
     sendGFIEmail(repoURL);
+    console.log(`Trying to send an email for ${repoURL}.`)
   });
 });
 cronJob.start();
