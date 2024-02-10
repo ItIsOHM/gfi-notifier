56cfe18f3e8f4c8642397a88ca9a534427dde3de
Rhythm Garg <rhythmgarg05@gmail.com>
2024-02-10 20:47:14 +0530

    authenticated queries for higher rate limit to github api

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const cron = require('cron');
-const nodemailer = require('nodemailer');
-const { type } = require('@testing-library/user-event/dist/type');
-require('dotenv').config();
-const {Resend} = require('resend');
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cron = require("cron");
+const nodemailer = require("nodemailer");
+const { type } = require("@testing-library/user-event/dist/type");
+require("dotenv").config();
+const { Resend } = require("resend");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,28 +16,26 @@ const DB_URL = process.env.DB_URI;
 app.use(express.json());
 app.use(cors());
 
-app.get('/', async (req, res) => {
-  res.json("Hello World.")
+app.get("/", async (req, res) => {
+  res.json("Hello World.");
 });
 
-app.post('/getGFI', async (req, res) => {
+app.post("/getGFI", async (req, res) => {
   const repo = req.query.repoURL;
   // console.log(repo);
   const apiUrl = `https://api.github.com/repos/${repo}/issues?labels=good%20first%20issue&&state=open`;
   // console.log(apiUrl);
   try {
-    const response = await axios.get(
-      apiUrl,
-      {
-        headers: {
-          'Authorization': process.env.GITHUB_ACCESS_TOKEN
-        }
-      });
+    const response = await axios.get(apiUrl, {
+      "headers": {
+        "Authorization" : "token " + `${process.env.GITHUB_ACCESS_TOKEN}`,
+      },
+    });
     const issues = response.data;
     // console.log(issues);
     res.status(200).json(issues);
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching issues in back' });
+    res.status(500).json({ error: "Error fetching issues in back" });
   }
 });
 
@@ -47,7 +45,7 @@ const subSchema = new mongoose.Schema({
   lastProcessedIssueId: Number,
 });
 
-const Subscription = mongoose.model('Subscription', subSchema);
+const Subscription = mongoose.model("Subscription", subSchema);
 
 const connectDatabase = async () => {
   try {
@@ -69,31 +67,34 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendGFIEmail = async (repoURL) => {
   const apiUrl = `https://api.github.com/repos/${repoURL}/issues?labels=good%20first%20issue&&state=open`;
   try {
-    const response = await axios.get(
-      apiUrl,
-      {
-        headers: {
-          'Authorization': process.env.GITHUB_ACCESS_TOKEN
-        }
-      });
+    const response = await axios.get(apiUrl, {
+      "headers": {
+        "Authorization" : "token " + `${process.env.GITHUB_ACCESS_TOKEN}`,
+      },
+    });
     const issues = response.data;
 
     try {
-      const subscribed = await Subscription.find({repoURL});
+      const subscribed = await Subscription.find({ repoURL });
 
-      if(issues.length > 0 && subscribed.length > 0) {
+      if (issues.length > 0 && subscribed.length > 0) {
         let lastProcessedIssueId = subscribed[0].lastProcessedIssueId || 0;
         const latestIssueID = issues[0].number;
 
-        if(latestIssueID > lastProcessedIssueId) {
+        if (latestIssueID > lastProcessedIssueId) {
           subscribed.forEach(async (sub) => {
-            if(sub.lastProcessedIssueId < latestIssueID) {
+            if (sub.lastProcessedIssueId < latestIssueID) {
               const subject = `A new GFI has been raised in ${repoURL}!`;
-              const text = `A new Good First Issue has been raised in the repository: ${repoURL}\n\nIssue Title: ${issues[0].title}\n\nIssue URL: ${(issues[0].url).replace("https://api.github.com/repos", "https://github.com/")}`;
+              const text = `A new Good First Issue has been raised in the repository: ${repoURL}\n\nIssue Title: ${
+                issues[0].title
+              }\n\nIssue URL: ${issues[0].url.replace(
+                "https://api.github.com/repos",
+                "https://github.com/"
+              )}`;
 
               try {
                 const mail = await resend.emails.send({
-                  from: 'Rhythm from GitAlert <rhythm@gitalert.me>',
+                  from: "Rhythm from GitAlert <rhythm@gitalert.me>",
                   to: sub.email,
                   subject: subject,
                   text: text,
@@ -112,63 +113,63 @@ const sendGFIEmail = async (repoURL) => {
       }
     } catch (error) {
       res.status(500).json({
-        error: `No subscribed repos found.`
-      })
+        error: `No subscribed repos found.`,
+      });
       console.log("Couldn't find subscribed repo: ", error);
     }
-  } catch(error) {
-    console.error('Error fetching GFIs:', error);
+  } catch (error) {
+    console.error("Error fetching GFIs:", error);
   }
 };
 
 const repoToCheck = [];
 const addRepoToCheck = (repoURL) => {
-  if(!repoToCheck.includes(repoURL)) {
+  if (!repoToCheck.includes(repoURL)) {
     repoToCheck.push(repoURL);
     console.log(`Added ${repoURL} to repoToCheck.`);
   }
 };
 
-const cronJob = new cron.CronJob('* * * * *', () => {
-  console.log('Checking for new GFIs...');
+const cronJob = new cron.CronJob("* * * * *", () => {
+  console.log("Checking for new GFIs...");
   repoToCheck.forEach((repoURL) => {
     sendGFIEmail(repoURL);
-    console.log(`Trying to send an email for ${repoURL}.`)
+    console.log(`Trying to send an email for ${repoURL}.`);
   });
 });
 cronJob.start();
 
-app.post('/subscribe', async (req, res) => {
+app.post("/subscribe", async (req, res) => {
   const repoURL = req.query.repoURL;
   const email = req.query.email;
   console.log(repoURL, email);
 
-  const existingSub = await Subscription.findOne({repoURL, email});
+  const existingSub = await Subscription.findOne({ repoURL, email });
 
-  if(existingSub) {
+  if (existingSub) {
     return res.status(500).json({
-      error : 'You are already subscribed to this repository.'
-    })
+      error: "You are already subscribed to this repository.",
+    });
   }
 
-  const newSub = new Subscription({repoURL,  email});
+  const newSub = new Subscription({ repoURL, email });
   await newSub.save();
 
   try {
     res.status(200).json({
-      message : 'Subscribed successfully! :D'
-    })
+      message: "Subscribed successfully! :D",
+    });
     addRepoToCheck(repoURL);
     await Subscription.findOneAndUpdate(
       { repoURL, email },
       { lastProcessedIssueId: 0 }
     );
-  } catch(error) {
+  } catch (error) {
     res.status(500).json({
-      error: `We're having an error subscibing you.`
-    })
+      error: `We're having an error subscibing you.`,
+    });
   }
-})
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
